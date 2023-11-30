64a4836bbfb87f55e8eecfb887d2e8a58bb0bf62
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2023-11-30 16:29:43 +0600

    Added footer using material UI

--- src/components/main/Footer.jsx
+++ src/components/main/Footer.jsx
@@ -2,12 +2,11 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import { NavLink } from 'react-router-dom';
 
 const useStyles = {
   footer: {
-    padding: '64px', 
-    backgroundColor: '#FBD38D', 
+    padding: '64px',
+    backgroundColor: '#FDE3C8', 
     color: '#ffffff',
   },
   footerTitle: {
@@ -15,18 +14,18 @@ const useStyles = {
     fontWeight: 'bold',
     marginBottom: '16px',
   },
+  linkContainer: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
   link: {
-    display: 'block',
-    color: '#ffffff',
+    color: '#7C7C7C', 
     textDecoration: 'none',
-    marginBottom: '8px',
+    marginRight: '16px',
     '&:hover': {
       textDecoration: 'underline',
     },
-  },
-  socialIcon: {
-    marginRight: '16px',
-    color: '#ffffff',
   },
 };
 
@@ -34,13 +33,21 @@ const Footer = () => {
   return (
     <footer style={useStyles.footer}>
       <Container>
-        <Grid container spacing={3} alignItems="left">
+        <Grid container spacing={3} alignItems="center">
           <Grid item xs={12} sm={6}>
-            <Typography variant="body2" align="right">
+            <Typography variant="body2">
               <span className="px-4 text-3xl font-semibold text-orange-600">
                 purr<span className="text-gray-600">fect </span>
                 <span className="font-normal text-xs md:text-sm"> Adoption Hub</span>
               </span>
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="body2" align="right" style={useStyles.linkContainer}>
+              <Link href="#" style={useStyles.link}>Careers</Link>
+              <Link href="#" style={useStyles.link}>Legal</Link>
+              <Link href="#" style={useStyles.link}>About</Link>
+              <Link href="#" style={useStyles.link}>Socials</Link>
             </Typography>
           </Grid>
         </Grid>
